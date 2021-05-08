02ff06aedb1a16f6f535397b9a9187f26f1a8825
Maximilian Leith <maximilian@leith.de>
2021-05-08 21:57:56 +0200

    added missing key to screens in split screen mode.

--- src/routes/Client/Clientwrapper.tsx
+++ src/routes/Client/Clientwrapper.tsx
@@ -1,63 +1,77 @@
-import { Container, Grid, GridSize, GridSpacing } from '@material-ui/core';
-import React, { Fragment, useState } from 'react';
-import Headbar from '../../components/Headbar/Headbar';
+import { Grid, GridSize } from "@material-ui/core";
+import { Fragment, useState } from "react";
+import Headbar from "../../components/Headbar/Headbar";
 import Client from "./Client";
 import AddClient from "../../components/AddClient/AddClient";
-import { toast } from 'react-toastify';
-import { useHistory } from 'react-router';
+import { toast } from "react-toastify";
 
 interface IClient {
-  setAuth(bool: boolean): void;
-  match: any;
+    setAuth(bool: boolean): void;
+    match: any;
+}
+
+interface ITab {
+    sessionID: number;
+    screenID: number;
 }
 
 export default function ClientWrapper(props: IClient) {
-  const id = props.match.params.id;
-  const [clientIds, setClientIds] = useState([id]);
+    let terminalCounter = 0;
 
-  let clientCount = clientIds.length;
+    const id = props.match.params.id;
+    const [clientIds, setClientIds] = useState([
+        { sessionID: id, screenID: ++terminalCounter } as ITab,
+    ]);
 
-  let sizes: GridSize[];
-  if (clientCount === 1) {
-    sizes = [12];
-  } else if (clientCount === 2) {
-    sizes = [6, 6];
-  } else if (clientCount === 3) {
-    sizes = [6, 6, 12];
-  } else {
-    sizes = [6, 6, 6, 6];
-  }
+    let clientCount = clientIds.length;
 
-  const addClientId = (id: number) => {
-    if (clientCount < 4) {
-      setClientIds([...clientIds, id]);
-      clientCount += 1;
+    let sizes: GridSize[];
+    if (clientCount === 1) {
+        sizes = [12];
+    } else if (clientCount === 2) {
+        sizes = [6, 6];
+    } else if (clientCount === 3) {
+        sizes = [6, 6, 12];
     } else {
-      toast.warning("You cant have more than 4 clients at the time");
+        sizes = [6, 6, 6, 6];
     }
-  }
 
-  const remove_client = (index: number) => {
-    let ids = [...clientIds];
-    console.log(ids);
-    ids.splice(index, 1);
-    console.log(ids);
-    setClientIds(ids);
-  }
-
-  return (
-    <Fragment>
-      <Headbar setAuth={props.setAuth} />
-      <Grid container direction="row" spacing={2} alignItems="flex-start">
-        {
-          clientIds.map((client: number, index:number) => (
-            <Grid item xs={12} md={sizes[index]}>
-              <Client id={client} clientCount={clientCount} selfDestroy={remove_client} index={index} />
-            </Grid>
-          ))
+    const addClientId = (id: number) => {
+        if (clientCount < 4) {
+            setClientIds([
+                ...clientIds,
+                { sessionID: id, screenID: ++terminalCounter },
+            ]);
+            clientCount += 1;
+        } else {
+            toast.warning("You can't have more than 4 clients at the time.");
         }
-      </Grid>
-      <AddClient add={addClientId} />
-    </Fragment>
-  )
+    };
+
+    const remove_client = (index: number) => {
+        let ids = [...clientIds];
+        console.log(ids);
+        ids.splice(index, 1);
+        console.log(ids);
+        setClientIds(ids);
+    };
+
+    return (
+        <Fragment>
+            <Headbar setAuth={props.setAuth} />
+            <Grid container direction="row" spacing={2} alignItems="flex-start">
+                {clientIds.map((client: ITab, index: number) => (
+                    <Grid item xs={12} md={sizes[index]} key={client.screenID}>
+                        <Client
+                            id={client.sessionID}
+                            clientCount={clientCount}
+                            selfDestroy={remove_client}
+                            index={index}
+                        />
+                    </Grid>
+                ))}
+            </Grid>
+            <AddClient add={addClientId} />
+        </Fragment>
+    );
 }
